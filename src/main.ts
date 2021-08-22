b2f591fce05679b356ca6fd5b8dc2d4ba74d8948
Bogdan Gladyshev <bogdan_gladyshev@epam.com>
2021-08-22 22:05:59 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import { ErrorMapper } from "utils/ErrorMapper";
+import { run as harvesterRun } from "role/harvester";
+
+declare global {
+  // Memory extension samples
+  interface Memory {
+    uuid: number;
+    log: any;
+  }
+
+  interface CreepMemory {
+    role: string;
+    room: string;
+    working: boolean;
+  }
+
+  // Syntax for adding proprties to `global` (ex "global.log")
+  namespace NodeJS {
+    interface Global {
+      log: any;
+    }
+  }
+}
+
+// When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
+// This utility uses source maps to get the line numbers and file names of the original, TS source code
+export const loop = ErrorMapper.wrapLoop(() => {
+  console.log(`Current game tick is ${Game.time}`);
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+          harvesterRun(creep);
+        }
+    }
+
+  // Automatically delete memory of missing creeps
+  for (const name in Memory.creeps) {
+    if (!(name in Game.creeps)) {
+      delete Memory.creeps[name];
+    }
+  }
+});
